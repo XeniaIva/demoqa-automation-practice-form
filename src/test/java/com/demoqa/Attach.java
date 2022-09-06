38b92f2e5af2f9f6906017a55979a989388c1129
19057857 <KAIvanushkina@sberbank.ru>
2022-09-06 18:28:04 +0300

    add attachments

--- src/test/java/com/demoqa/Attach.java
+++ src/test/java/com/demoqa/Attach.java
@@ -36,7 +36,7 @@ public class Attach {
                 String.join("\n", Selenide.getWebDriverLogs(BROWSER))
         );
     }
-gi
+
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
